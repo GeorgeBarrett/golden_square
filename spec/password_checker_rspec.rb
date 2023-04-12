994c8c26e8f4f922efd18d82fdb9f767a5d117d7
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-12 14:47:33 +0100

    pass-test has been added to the test suite

--- spec/password_checker_rspec.rb
+++ spec/password_checker_rspec.rb
@@ -7,4 +7,14 @@ RSpec.describe PasswordChecker do
       expect { password_checker.check() }.to raise_error "Invalid password, must be 8+ characters."
     end
   end
+
+  context "when password is more or equal to 8 characters" do
+    it "passes" do
+      password_checker = PasswordChecker.new
+      password_checker.check("password")
+      expect(result).to eq true
+    end
+  end
 end
+
+
